96d99daaf0096f129576aaa9b285bf384ca678bc
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2015-07-20 00:06:06 +0900

    Added upper keyboard (E key = C5)

--- src/VirtualKeyboard.js
+++ src/VirtualKeyboard.js
@@ -7,21 +7,28 @@ export default class VirtualKeyboard {
 			this.keyState[i] = false;
 		}
 
-		let keys = [67, 70, 86, 71, 66, 78, 74, 77, 75, 188, 76, 190, 191];
+		let lowerKeys = [67, 70, 86, 71, 66, 78, 74, 77, 75, 188, 76, 190, 191];
+		let upperKeys = [69, 52, 82, 53, 84, 89, 55, 85, 56, 73, 57, 79, 80];
 
 		this.key2note = {};
 
-		let note = 60;
-		for (let i = 0; i < keys.length; i++) {
-			this.key2note[keys[i]] = note;
-			note++;
+		for (let i = 0; i < lowerKeys.length; i++) {
+			this.key2note[lowerKeys[i]] = 60 + i;
+		}
+		for (let i = 0; i < upperKeys.length; i++) {
+			this.key2note[upperKeys[i]] = 72 + i;
 		}
 		
 		document.body.addEventListener("keydown", event => this.onKeyDown(event));
 		document.body.addEventListener("keyup", evemt => this.onKeyUp(event));
+		
+		// this.keys = [];
 	}
 	
 	onKeyDown(event) {
+		// this.keys.push(event.keyCode);
+		// console.log(this.keys);
+		
 		let note = this.key2note[event.keyCode];
 		if (this.keyState[note] === false) {
 			this.keyState[note] = true;
