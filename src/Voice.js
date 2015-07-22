264a96f4a2ef924feae805b8d7b71d757aa3557d
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2015-07-23 01:06:05 +0900

    Create oscillators

--- src/Voice.js
+++ src/Voice.js
@@ -1,3 +1,6 @@
+import SquareOscillator from "./oscillators/SquareOscillator";
+import TriangleOscillator from "./oscillators/TriangleOscillator";
+
 const STATE_OFF = 0;
 const STATE_ATTACK = 1; // not used
 const STATE_DECAY = 2; // not used
@@ -6,14 +9,18 @@ const STATE_RELEASE = 4;
 
 export default class Voice {
 	constructor() {
-		this.state = STATE_OFF;
+		this.state = STATE_OFF; 
 	}
 	
 	play(note) {
 		this.state = STATE_SUSTAIN;
 		this.note = note;
+		this.frequency = 440 * Math.pow(2, (note - 69) / 12);
 		this.volume = 1;
 		this.phase = 0;
+		
+		this.oscillator = new SquareOscillator();
+		// this.oscillator = new TriangleOscillator();
 	}
 	
 	stop() {
@@ -26,7 +33,7 @@ export default class Voice {
 			let period = sampleRate / frequency;
 	
 			for (let i = 0; i < buffer.length; i++) {
-				buffer[i] += Math.sin(this.phase * (Math.PI * 2)) * this.volume * 0.1;
+				buffer[i] += this.oscillator.getSample(this.phase) * this.volume * 0.1;
 				this.phase += 1 / period;
 				
 				if (this.state === STATE_RELEASE) {
