0ec94ee51a7790288880241284fd12a6dacaeb04
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2015-07-29 00:04:00 +0900

    Draw keyboard

--- src/VirtualKeyboard.js
+++ src/VirtualKeyboard.js
@@ -23,6 +23,8 @@ export default class VirtualKeyboard {
 		document.body.addEventListener("keyup", evemt => this.onKeyUp(event));
 		
 		this.damperPedal = false;
+		
+		this.createKeyboard();
 	}
 	
 	onKeyDown(event) {
@@ -38,7 +40,7 @@ export default class VirtualKeyboard {
 		let note = this.key2note[event.keyCode];
 		if (this.keyState[note] === false) {
 			this.keyState[note] = true;
-			this.synthesizer.processMIDIMessage([0x90, note, 96]);
+			this.noteOn(note);
 		}
 	}
 	
@@ -51,7 +53,96 @@ export default class VirtualKeyboard {
 		let note = this.key2note[event.keyCode];
 		if (this.keyState[note] === true) {
 			this.keyState[note] = false;
-			this.synthesizer.processMIDIMessage([0x80, note, 96]);
+			this.noteOff(note);
+		}
+	}
+	
+	noteOn(note, velocity = 96) {
+		this.synthesizer.processMIDIMessage([0x90, note, velocity]);
+	}
+	
+	noteOff(note) {
+		this.synthesizer.processMIDIMessage([0x80, note, 0]);
+	}
+	
+	createKeyboard() {
+		const KEY_LENGTH = 120;
+		
+		let parent = document.getElementById("keyboard");
+		
+		let canvas = document.createElement("canvas");
+		canvas.setAttribute("width", "1000");
+		canvas.setAttribute("height", "" + KEY_LENGTH);
+		parent.appendChild(canvas);
+		
+		const KEYS = [
+			{ dx: 0.4, black: false },	// C
+			{ dx: 0.6, black: true },	// C#
+			{ dx: 0.6, black: false },	// D
+			{ dx: 0.4, black: true },	// D#
+			{ dx: 1.0, black: false },	// E
+			{ dx: 0.35, black: false },	// F
+			{ dx: 0.65, black: true },	// F#
+			{ dx: 0.5, black: false },	// G
+			{ dx: 0.5, black: true },	// G#
+			{ dx: 0.65, black: false },	// A
+			{ dx: 0.35, black: true },	// A#
+			{ dx: 1.0, black: false }	// B
+		];
+		
+		let wholeToneInterval = 28; // pixel
+		
+		let context = canvas.getContext("2d");
+		
+		const LOWEST_NOTE = 21;
+		const HIGHEST_NOTE = 21 + 88;
+		const START_KEY = 9;
+		
+		// draw white keys
+		let x = 0.5;
+		let key = START_KEY;
+		
+		for (let note = LOWEST_NOTE; note < HIGHEST_NOTE; note++) {
+			if (!KEYS[key].black) {
+				let center = x * wholeToneInterval;
+				let width = wholeToneInterval;
+				let height = KEY_LENGTH;
+				
+				context.fillStyle = "#F8F8F8";
+				context.fillRect(center - width / 2, 0, width, height);
+				
+				context.lineWidth = 0.5;
+				context.strokeStyle = "#CCC";
+				context.beginPath();
+				context.moveTo(center - width / 2 - 0.5, 0);
+				context.lineTo(center - width / 2 - 0.5, KEY_LENGTH);
+				context.stroke();
+			}
+			
+			x += KEYS[key].dx;
+			if (++key >= KEYS.length) {
+				key = 0;
+			}
+		}
+		
+		// draw black keys
+		x = 0.5;
+		key = START_KEY;
+		
+		for (let note = LOWEST_NOTE; note < HIGHEST_NOTE; note++) {
+			if (KEYS[key].black) {
+				let center = x * wholeToneInterval;
+				let width = wholeToneInterval * 0.5;
+				let height = KEY_LENGTH * 0.625;
+				
+				context.fillStyle = "#333";
+				context.fillRect(center - width / 2, 0, width, height);
+			}
+			
+			x += KEYS[key].dx;
+			if (++key >= KEYS.length) {
+				key = 0;
+			}
 		}
 	}
 }
