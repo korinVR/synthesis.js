231476cf928dca5628afd529d1a9b3a21a2b2bab
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2015-08-02 04:21:31 +0900

    Support SMF format 1 and multitrack

--- src/SMFPlayer.js
+++ src/SMFPlayer.js
@@ -12,21 +12,43 @@ export default class SMFPlayer {
 		this.smf = smf;
 		
 		// read SMF header
-		this.pos = 8;
+		let pos = 8;
 		
-		let format = this.read2bytes();
-		let trackNumber = this.read2bytes();
-		let resolution = this.read2bytes();
+		function read2bytes() {
+			return smf[pos++] << 8 | smf[pos++];
+		}
 		
-		if (format === 0 && trackNumber !== 1) {
+		function read4bytes() {
+			return smf[pos++] << 24 | smf[pos++] << 16 | smf[pos++] << 8 | smf[pos++];
+		}
+		
+		let format = read2bytes();
+		this.trackNumber = read2bytes();
+		this.resolution = read2bytes();
+		
+		if (format === 0 && this.trackNumber !== 1) {
 			throw new Error("illegal track number");
 		}
 		
-		this.trackDataLength = this.read4bytes();
-		this.trackEndPos = this.trackStartPos + this.trackDataLength;
+		this.trackPos = [];
+		this.trackEndPos = [];
+		this.nextEventTick = [];
 		
-		this.startTime = Date.now();
-		this.nextEventTime = this.tick2ms(this.readByte());
+		// read track headers
+		for (let i = 0; i < this.trackNumber; i++) {
+			pos += 4;
+			
+			let trackDataLength = read4bytes();
+			this.trackPos[i] = pos;
+			this.trackEndPos[i] = pos + trackDataLength;
+			this.nextEventTick[i] = this.readDeltaTick(i);
+			
+			pos += trackDataLength;
+		}
+		
+		// set up timer
+		this.prevTime = Date.now();
+		this.currentTick = 0;
 		
 		if (!this.intervalId) {
 			this.intervalId = setInterval(() => this.onInterval(), INTERVAL);
@@ -39,51 +61,42 @@ export default class SMFPlayer {
 	}
 	
 	onInterval() {
-		let currentTime = Date.now() - this.startTime;
+		// calclate delta time
+		let currentTime = Date.now();
+		let deltaTime = currentTime - this.prevTime;
+		this.prevTime = currentTime; 
 		
-		while (this.nextEventTime < currentTime) {
-			// send MIDI message
-			let statusByte = this.readByte();
-			let dataByte1 = this.readByte();
-			let dataByte2 = this.readByte();
+		function time2tick(time) {
+			let quarterTime = 60 * 1000 / this.tempo;
+			let tickTime = quarterTime / this.resolution;
 			
-			this.synthesizer.processMIDIMessage([statusByte, dataByte1, dataByte2]);
-			
-			if (this.trackPos >= this.trackEndPos) {
-				// end of track data
-				this.stop();
-				break;
+			return time / tickTime;
+		}
+		
+		this.currentTick += time2tick(deltaTime);
+		
+		for (let i = 0; i < this.trackNumber; i++) {
+			while (this.nextEventTick[i] < this.currentTick) {
+				// send MIDI message
+				let statusByte = this.smf[this.trackPos[i]++];
+				let dataByte1 = this.smf[this.trackPos[i]++];
+				let dataByte2 = this.smf[this.trackPos[i]++];
+
+				this.synthesizer.processMIDIMessage([statusByte, dataByte1, dataByte2]);
+
+				if (this.trackPos[i] >= this.trackEndPos) {
+					// end of track data
+					this.stop();
+					break;
+				}
+				
+				// calculate next event tick
+				this.nextEventTick[i] += this.readDeltaTick(i);
 			}
-			
-			// calculate next event time
-			let deltaTick = this.readByte();
-			this.nextEventTime += this.tick2ms(deltaTick);
 		}
 	}
 	
-	readByte() {
-		return this.smf[this.pos++];
-	}
-	
-	read2bytes() {
-		let length =
-			this.smf[this.pos++] << 8 |
-			this.smf[this.pos++]
-		
-		return length;
-	}
-	
-	read4bytes() {
-		let length =
-			this.smf[this.pos++] << 24 |
-			this.smf[this.pos++] << 16 |
-			this.smf[this.pos++] << 8 |
-			this.smf[this.pos++];
-		
-		return length;
-	}
-	
-	tick2ms(tick) {
-		return 60 * 1000 / this.tempo * tick / this.resolution;
+	readDeltaTick(track) {
+		return this.smf[this.trackPos[track]++];
 	}
 }
